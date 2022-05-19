49b2ba86a9c9a701bba80e54f7fcb19b35d94d2b
Trevor Whittingham <twhitt14@gmail.com>
2022-05-19 15:58:56 -0600

    fix test name

--- Test HelpersTests/MemoryLeakTests.swift
+++ Test HelpersTests/MemoryLeakTests.swift
@@ -12,7 +12,7 @@ class MemoryLeakTests: XCTestCase {
     
     // Feel free to add this `trackForMemoryLeaks` call at the end of every test, making sure the SUT is able to be deallocated after it's no longer used.
 
-    func test_ViewController_MemoryLeak() throws {
+    func test_SomeClass_MemoryLeak() throws {
         let sut = SomeClass()
         
         trackForMemoryLeaks(sut)
